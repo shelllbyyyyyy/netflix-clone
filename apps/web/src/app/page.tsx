8808f9f131b2e0aa080729b1c6c244313564f18d
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-11-28 17:36:54 +0700

    ref: migrate to src folder

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-6xl">Netflix clone</h1>
+    </main>
+  );
+}

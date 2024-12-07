f59f141c2dc320f4df4e376a16c0c37fcd38b757
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-12-07 16:03:53 +0700

    chore: add fetch cloudinary image

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
